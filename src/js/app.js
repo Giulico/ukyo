a073149db246bfd3cf3ff2bd47fdc06327cb6a03
giuliocollesei <giuliocollesei@gmail.com>
2020-07-17 15:17:34 +0200

    added PRETTIER

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
-import "../scss/app.scss";
-import "../pug/index.pug";
+import '../scss/app.scss'
+import '../pug/index.pug'
 
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
-const myArray = [1, 2, 3];
+const myArray = [1, 2, 3]
 
-console.log("Hi, my name is Ukyo!", myArray.includes(2)); // eslint-disable-line no-console
+console.log('Hi, my name is Ukyo!', myArray.includes(2)) // eslint-disable-line no-console
